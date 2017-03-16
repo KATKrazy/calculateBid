099558195e7e017661b8ea61103b78b5c4039775
ke_lu <410231238@qq.com>
2017-03-16 19:55:48 +0800

    提交service和单元测试

---
+++ calcBid/src/main/java/com/katkrazy/calculate/service/CalculateService.java
@@ -0,0 +1,11 @@
+package com.katkrazy.calculate.service;
+
+/**
+ * Created by kingdee on 2017/3/16.
+ */
+public class CalculateService {
+
+    public CalculateService() {
+        System.out.print("init!!!!!");
+    }
+}
---
+++ calcBid/src/test/com.katkrazy.calculate.action.test/CalculateActionTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by kingdee on 2017/3/16.
+ */
+import com.katkrazy.calculate.action.CalculateAction;
+import org.junit.Test;
+
+public class CalculateActionTest {
+
+    @Test
+    public void testExecute() {
+        CalculateAction ca = new CalculateAction();
+    }
+}
